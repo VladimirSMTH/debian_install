93d9a737072724634f5f1a3d5befe02dae09de51
kvacompservice <107845375+kvacompservice@users.noreply.github.com>
2022-06-20 13:08:34 +0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -11,6 +11,6 @@ sudo apt install mariadb-server mariadb-client -y
 sudo systemctl enable mariadb
 curl -sSL https://packages.sury.org/php/README.txt | sudo bash -x
 sudo apt upgrade -y
-sudo apt install php8.1 libapache2-mod-php8.1
+sudo apt install php8.1 libapache2-mod-php8.1 -y
 sudo a2enmod php8.1
 sudo systemctl restart apache2
