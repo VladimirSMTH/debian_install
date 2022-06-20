3e4bb0fa39602180983fce346d6e2fee6f122ca0
kvacompservice <107845375+kvacompservice@users.noreply.github.com>
2022-06-20 18:01:40 +0300

    Update zabbix.sh

--- docker/zabbix.sh
+++ docker/zabbix.sh
@@ -6,11 +6,13 @@ docker run --name mysql-server -t \
       -e MYSQL_USER="zabbix" \
       -e MYSQL_PASSWORD="zabbix_pwd" \
       -e MYSQL_ROOT_PASSWORD="root_pwd" \
+      --network=zabbix-net \
       -d mysql:8.0 \
       --restart unless-stopped \
       --character-set-server=utf8 --collation-server=utf8_bin \
       --default-authentication-plugin=mysql_native_password
 docker run --name zabbix-java-gateway -t \
+      --network=zabbix-net \
       --restart unless-stopped \
       -d zabbix/zabbix-java-gateway:alpine-5.4-latest
 docker run --name zabbix-server-mysql -t \
@@ -20,6 +22,7 @@ docker run --name zabbix-server-mysql -t \
       -e MYSQL_PASSWORD="zabbix_pwd" \
       -e MYSQL_ROOT_PASSWORD="root_pwd" \
       -e ZBX_JAVAGATEWAY="zabbix-java-gateway" \
+      --network=zabbix-net \
       -p 10051:10051 \
       --restart unless-stopped \
       -d zabbix/zabbix-server-mysql:alpine-5.4-latest
@@ -30,6 +33,7 @@ docker run --name zabbix-web-nginx-mysql -t \
       -e MYSQL_USER="zabbix" \
       -e MYSQL_PASSWORD="zabbix_pwd" \
       -e MYSQL_ROOT_PASSWORD="root_pwd" \
+      --network=zabbix-net \
       -p 80:8080 \
       --restart unless-stopped \
       -d zabbix/zabbix-web-nginx-mysql:alpine-5.4-latest
