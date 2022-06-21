b1f9f1cfab60fe87f01c7568bd066eb7ab2703c6
kvacompservice <107845375+kvacompservice@users.noreply.github.com>
2022-06-21 09:56:43 +0300

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -4,6 +4,6 @@ sudo mkdir /var/lib/prometheus
 curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest|grep browser_download_url|grep linux-amd64|cut -d '"' -f 4|wget -qi -
 tar -xvf prometheus-2.36.2.linux-amd64.tar.gz
 sudo mv prometheus-2.36.2.linux-amd64 /etc/prometheus
-sudo cp /etc/prometheus/prometheus /usr/local/bin/
-sudo cp /etc/prometheus/promtool /usr/local/bin/
+sudo cp /etc/prometheus/prometheus-2.36.2.linux-amd64/prometheus /usr/local/bin/
+sudo cp /etc/prometheus/prometheus-2.36.2.linux-amd64/promtool /usr/local/bin/
 sudo systemctl enable --now prometheus
