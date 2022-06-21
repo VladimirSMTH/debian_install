95795097e92f61b393b8308816d6e0238221b992
kvacompservice <107845375+kvacompservice@users.noreply.github.com>
2022-06-21 10:06:49 +0300

    Update prometheus.sh

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -6,5 +6,6 @@ tar -xvf prometheus-2.36.2.linux-amd64.tar.gz
 sudo mv prometheus-2.36.2.linux-amd64 /etc/prometheus
 sudo cp /etc/prometheus/prometheus-2.36.2.linux-amd64/prometheus /usr/local/bin/
 sudo cp /etc/prometheus/prometheus-2.36.2.linux-amd64/promtool /usr/local/bin/
+wget -O /etc/systemd/system/prometheus.service https://raw.githubusercontent.com/kvacompservice/debian_install/main/prometheus/prometheus.service
 sudo systemctl daemon-reload
 sudo systemctl enable --now prometheus
