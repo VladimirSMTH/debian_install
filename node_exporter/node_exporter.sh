9189dcf0e882b3f1fd9df1749a7227bf27246f31
kvacompservice <107845375+kvacompservice@users.noreply.github.com>
2022-06-23 10:01:59 +0300

    Update node_exporter.sh

--- node_exporter/node_exporter.sh
+++ node_exporter/node_exporter.sh
@@ -4,7 +4,7 @@ tar xvfz node_exporter-1.3.1.linux-amd64.tar.gz
 cd node_exporter-1.3.1.linux-amd64 
 mv node_exporter /usr/local/bin/
 sudo useradd -r -M -s /bin/false node_exporter
-wget -o /etc/systemd/system/node_exporter.service https://raw.githubusercontent.com/kvacompservice/debian_install/main/node_exporter/node_exporter.service
+wget -P /etc/systemd/system/ https://raw.githubusercontent.com/kvacompservice/debian_install/main/node_exporter/node_exporter.service
 chown -R node_exporter:node_exporter /usr/local/bin/node_exporter
 sudo systemctl daemon-reload 
 sudo systemctl start node_exporter
